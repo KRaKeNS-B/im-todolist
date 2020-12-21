8c495330d1eb59116449a01e2adc2608b78cd7a5
a.prokudin <a.prokudin@involta.ru>
2020-12-21 13:33:29 +0300

    task list undefined fix

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,7 @@ export default new Vuex.Store({
       state.newTask.text = text;
     },
     updateTaskList(state, taskList) {
-      state.taskList = taskList;
+      state.taskList = taskList || [];
     },
   },
   actions: {
